4874c898ce027026bbb332375e53ba164d6f7222
Tom Bilbe <tbilbe@gmail.com>
2018-03-18 10:27:31 +0000

    cleaned up app.js file - added comments

--- app.js
+++ app.js
@@ -5,42 +5,32 @@ var Knwl = require('knwl.js');
 var knwlInstance = new Knwl('english');
 knwlInstance.register('dates', require('./node_modules/knwl.js/default_plugins/dates'));
 
-var searchCompany = splitEmailDown('test@gmail.com');
+var searchCompany = splitEmailDown('test@canddi.com');
+// Create URL string - either .com or .co.uk
 var URL = 'https://www.' + searchCompany + '.co.uk/';
 var URLCom = 'https://www.' + searchCompany + '.com/';
-var URLhelp = 'https://www.' + searchCompany + '.co.uk/help';
-// var URLcontactUs = 'https://www.' + searchCompany + '.co.uk/contactsus';
-var URLabout = 'https://www.' + searchCompany + '.co.uk/about';
 
-request(URL, function (error, response, body) {
+// Optional URL strings where emails and address for contacts could be held?
+// var URLhelp = 'https://www.' + searchCompany + '.co.uk/help';
+// var URLcontactUs = 'https://www.' + searchCompany + '.co.uk/contactsus';
+// var URLabout = 'https://www.' + searchCompany + '.co.uk/about';
+
+request(URLCom, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     var $ = cheerio.load(body);
     // get the page source of the searched company page
     var pageSource = $.html();
-    var contactPageExists = pageSource.search('contact');
-    console.log(pageSource);
-    // var glove = $('a').text();
+    // console.log(pageSource);
 
-    // console.log(/(con)\w+/g.exec(glove));
-    // var URL_ContactPage = URL + glove;
-    // console.log(URL_ContactPage);
-
-    // knwlInstance.init(pageSource);
-    // var output = knwlInstance.get('emails');
-    // console.log(output);
-  } else {
-    console.log('-> checking .com address...')
-    // if error comes back try URL with .com
-    request(URLCom, function (error, response, body) {
-      var $ = cheerio.load(body);
-      // get the page source of the searched company page
-      var pageSource = $.html();
-      // console.log(pageSource);
-      knwlInstance.init(pageSource);
-      var output = knwlInstance.get('emails');
-      console.log(output);
-    })
-
+    knwlInstance.init(pageSource);
+    var outputPhones = knwlInstance.get('phones');
+    var outputLinks = knwlInstance.get('links');
+    var outputEmails = knwlInstance.get('emails');
+    var outputAddress = knwlInstance.get('places');
+    console.log('Output searching for phones: ' + JSON.stringify(outputPhones));
+    console.log('Output searching for links: ' + JSON.stringify(outputLinks));
+    console.log('Output searching for emails: ' + JSON.stringify(outputEmails));
+    console.log('Output searching for addresses: ' + JSON.stringify(outputAddress));
   }
 });
 
@@ -50,10 +40,11 @@ function splitEmailDown(emailInput) {
   var companyString;
   var re = /@(\w+)/gi;
   companyString = re.exec(emailInput)[1];
-
-  // console.log(companyString);
-  //   //check if email is personal gmail, hotmail, outlook ect
-  if (companyString === 'gmail' || companyString === 'hotmail' || companyString === 'outlook') {
+  console.log(companyString);
+  //check if email is personal gmail, hotmail, outlook ect
+  if (companyString === 'gmail' ||
+    companyString === 'hotmail' ||
+    companyString === 'outlook') {
     console.log(emailInput + ': Leads to a personal Email provided by - ' + companyString);
     console.log('Input another email address to test');
     return false;
@@ -62,18 +53,6 @@ function splitEmailDown(emailInput) {
     return companyString;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // splitEmailDown('tom@Deerns.com');
 // splitEmailDown('test@hotmail.com');
